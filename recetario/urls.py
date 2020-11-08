28c13a55bc7c72a2035352b076cb5e2e6ed82a60
daniepusb <danie.p.usb@gmail.com>
2020-11-08 18:18:03 -0300

    se crea el view de recetas

--- recetario/urls.py
+++ recetario/urls.py
@@ -2,14 +2,16 @@
 #from django.contrib import admin
 from django.urls import path
 
-from recetario import views
+from recetario import views as local_views
+from recipes import views as recipes_views
 
 
 urlpatterns = [
    #path('admin/', admin.site.urls),        #http://127.0.0.1:8000/admin/
-    path('', views.recipes),#http://127.0.0.1:8000/recipes
-    path('hello-world/', views.hello_world),#http://127.0.0.1:8000/hello-world/
-    path('sorted/', views.sort_integers),   #http://127.0.0.1:8000/sorted
-    path('hi/<str:name>/<int:age>/', views.say_hi), #http://127.0.0.1:8000/hi/jesus/33
+    path('', local_views.recipes),#http://127.0.0.1:8000/recipes
+    path('hello-world/', local_views.hello_world),#http://127.0.0.1:8000/hello-world/
+    path('sorted/', local_views.sort_integers),   #http://127.0.0.1:8000/sorted
+    path('hi/<str:name>/<int:age>/', local_views.say_hi), #http://127.0.0.1:8000/hi/jesus/33
 
+    path('recipes/', recipes_views.list_recipes),  #http://127.0.0.1:8000/recipes
 ]
--- recipes/apps.py
+++ recipes/apps.py
@@ -1,5 +1,10 @@
+"""Recipes application module."""
+
 from django.apps import AppConfig
 
 
 class RecipesConfig(AppConfig):
+    """Recipes application settings."""
+
     name = 'recipes'
+    varbose_name = 'Recipes'
